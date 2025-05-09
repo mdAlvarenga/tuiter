00c1b0344facdfae3e12a8df425c6f23f6966cb1
Marcos Alvarenga <alvarenga.marcos.daniel@gmail.com>
2025-05-09 12:46:29 -0300

    Initial commit

---
+++ src/test/java/com/challenge/tuiter/TuiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.tuiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
