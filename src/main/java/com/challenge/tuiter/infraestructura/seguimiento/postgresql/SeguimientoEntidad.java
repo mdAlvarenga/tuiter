3ffb0182200c0ddc860183df7e1a1ca14d311893
Marcos Alvarenga <alvarenga.marcos.daniel@gmail.com>
2025-05-10 16:01:28 -0300

    No permitir campos nulos en la db

--- src/main/java/com/challenge/tuiter/infraestructura/seguimiento/postgresql/SeguimientoEntidad.java
+++ src/main/java/com/challenge/tuiter/infraestructura/seguimiento/postgresql/SeguimientoEntidad.java
@@ -1,23 +1,28 @@
 package com.challenge.tuiter.infraestructura.seguimiento.postgresql;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Entity
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "seguimientos")
 public class SeguimientoEntidad {
   @Id
   private UUID id;
+
+  @Column(nullable = false)
   private String seguidorId;
+
+  @Column(nullable = false)
   private String seguidoId;
 
 }
