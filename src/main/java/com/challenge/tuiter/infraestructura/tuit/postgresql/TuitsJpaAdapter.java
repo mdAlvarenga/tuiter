ca8254e3f96d4ce74b59ea9ca08d5bbbb06e731b
Marcos Alvarenga <alvarenga.marcos.daniel@gmail.com>
2025-05-11 13:48:49 -0300

    Configuración para levantar con h2 local

--- src/main/java/com/challenge/tuiter/infraestructura/tuit/postgresql/TuitsJpaAdapter.java
+++ src/main/java/com/challenge/tuiter/infraestructura/tuit/postgresql/TuitsJpaAdapter.java
@@ -3,12 +3,14 @@ package com.challenge.tuiter.infraestructura.tuit.postgresql;
 import com.challenge.tuiter.dominio.tuit.RepositorioDeBusquedaTuits;
 import com.challenge.tuiter.dominio.tuit.RepositorioDeGuardadoTuits;
 import com.challenge.tuiter.dominio.tuit.Tuit;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
+@Primary
 public class TuitsJpaAdapter implements RepositorioDeGuardadoTuits, RepositorioDeBusquedaTuits {
   private final TuitJpaRepository jpa;
 
